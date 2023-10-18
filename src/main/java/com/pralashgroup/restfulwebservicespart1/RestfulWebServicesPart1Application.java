626e463be6b4449fefa947e68ba34690063e4834
nahakritesh06 <nahakritesh06@gmail.com>
2023-10-18 21:35:54 +0530

    "Initial change"

---
+++ src/main/java/com/pralashgroup/restfulwebservicespart1/RestfulWebServicesPart1Application.java
@@ -0,0 +1,13 @@
+package com.pralashgroup.restfulwebservicespart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pralashgroup/restfulwebservicespart1/RestfulWebServicesPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pralashgroup.restfulwebservicespart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
